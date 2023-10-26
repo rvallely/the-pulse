9d517ed0ea0b49ce1ec7199168bcd1c5491fd3ed
rvallely <rosievallely@hotmail.com>
2023-10-26 13:25:10 +0100

    allow user to log out

--- src/components/User/UserArea/UserArea.jsx
+++ src/components/User/UserArea/UserArea.jsx
@@ -2,13 +2,13 @@ import Header from "../../Header/Header";
 import Nav from "../../Header/Nav";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../../../contexts/User";
+import { UserContext, defaultUserContext } from "../../../contexts/User";
 import commentsIcon from '../../../assets/icons/comments.png';
 import articlesIcon from '../../../assets/icons/articles.png';
 import postArticleIcon from '../../../assets/icons/postArticle.png';
 import logOutIcon from '../../../assets/icons/logOut.png';
 
-function UserArea(params) {
+function UserArea() {
     const { loggedInUser: { username }, setLoggedInUser } = useContext(UserContext);
 
     const navigate = useNavigate(); 
@@ -22,7 +22,7 @@ function UserArea(params) {
             <div className="grid-container user-area-grid-container">
                 <div
                     className="grid-item topic-user-area-grid-item user-area-grid-item1"
-                    // onClick={() => navigate(`/articles?topic=${name}`)}
+                    onClick={() => navigate(`/comments?page=0`)}
                 >
                     <p className='topic-grid-title-font text-align-centre' style={{ width: '100%'}}>
                         My Comments
@@ -65,9 +65,9 @@ function UserArea(params) {
                 <div
                     className="grid-item topic-user-area-grid-item user-area-grid-item4"
                     onClick={() => {
-                        // Log user out
-                        // Navigate to home screen or login?
-                        // navigate(`/user/post-article`)
+                        localStorage.removeItem('loggedInUser');
+                        setLoggedInUser(defaultUserContext);
+                        navigate('/');
                     }}
                 >
                 <p className='topic-grid-title-font text-align-centre' style={{ width: '100%'}}>
--- src/contexts/User.js
+++ src/contexts/User.js
@@ -1,8 +1,10 @@
 import { createContext } from 'react';
 
-export const UserContext = createContext({
+export const defaultUserContext = {
   username: 'Log in',
   avatar_url:
     'https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png'
-});
+};
+
+export const UserContext = createContext(defaultUserContext);
 
